7df6dae74f4da133e5f64cf8ea19ae54633c6a0a
crissthiandi <albertocenaa@gmail.com>
2020-12-28 21:08:31 -0600

    Esqueleto de Documentación de Roxigen2: no se documento la .rd por que no compilo

--- R/series_de_tiempo.R
+++ R/series_de_tiempo.R
@@ -567,9 +567,9 @@ dev.TRS <- function(){
 #' @param init_ Boleano, True/False indica di se vericaran los datos
 #'
 #' @return  La salida no es como tal un objeto, si no una serie de impresiones de varios
-#'   varios analisis.
-#'   \itemize{\item{Plost}{  Arroja una lista de plots que ayudan a ver el comportamiento de la serie y como ciertos ajustes se aproximan mejor a ella}}
-#'   \itemize{\item{PNG}{ Imagenes grabadas en el directorio de trabajo}}
+#'    analisis.
+#'    \itemize{\item{Plost}{  Arroja una lista de plots que ayudan a ver el comportamiento de la serie y como ciertos ajustes se aproximan mejor a ella}}
+#'    \itemize{\item{PNG}{ Imagenes grabadas en el directorio de trabajo}}
 #'
 #' @author Cristhian Diaz
 #'
@@ -1009,6 +1009,30 @@ recomendacion_autocorrelaciones <- function(objeto_cf,print_IC=FALSE) {
   return(invisible(posibles_lags[length(posibles_lags)]))
 }
 
+#' Ajuste de Modelo ARIMA a tu Serie de tiempo
+#'
+#' Ajusta un modelo arima usando un asistente que te mostrar si tu serie se ajusta a un modelo ARIMA en particular
+#'
+#' A diferencia de otras funciones esta utiliza el conocimiento del experto para ajustar el mejor modelo ARIMA a la serie de tiempo estudiada, mientras propone modelos que el asistente considera utiles.
+#'
+#' @param datos Dataframe de no mas de 2 columnas, en el orden primero tiempo y
+#'    luego valor el tiempo va en formato fecha, y tiene que ser en el orden
+#'    dia-mes-year. La versión 2.1 soporta objetos de la clase TS (time series)
+#' @param frecuencia  Este es el periodo de la serie, trimestral = 3, cuatrimestral = 4
+#'    ,mensual = 12, etc.
+#' @param inicio Este es el year a iniciar la serie de tiempo
+#' @param init_ Boleano, True/False indica di se vericaran los datos
+#'
+#' @return La salida no es como tal un objeto, si no una serie de impresiones de varios
+#'    analisis.
+#' @export
+#'
+#' @examples
+#' serie_tiempo_ARIMA(sunspot.year,5)
+#'
+#' base=data.frame(tiempo=seq(Sys.Date(),by="days",length=20),valores=1:20*3+runif(1))
+#' serie_tiempo_ARIMA(datos=base,frecuencia=4,inicio=2010)
+#'
 serie_tiempo_ARIMA<-function(datos,frecuencia=NULL,inicio=NULL,init_=FALSE){
   if(!init_){
     paquetes.tsrutina()
